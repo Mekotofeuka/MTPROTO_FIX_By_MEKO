ba589fca7d9b6f069442ba6a3341ff3c91b2845d
Meko <137619369+Mekotofeuka@users.noreply.github.com>
2026-07-11 19:48:52 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 BASE_URL="https://raw.githubusercontent.com/Mekotofeuka/MTPROTO_FIX_By_MEKO/main"
-FILES=("main.sh" "proxys/proxymenu.sh" "proxys/telemt1.sh" "proxys/mtprotozig1.sh" "proxys/telemt_in_docker1.sh" "proxy_checker.py")
+FILES=("main.sh" "proxys/proxymenu.sh" "proxys/telemt1.sh" "proxys/mtprotozig1.sh" "proxys/mtgv2_1.sh" "proxys/telemt_in_docker1.sh" "proxy_checker.py")
 
 # ── Цвета ─────────────────────────────────────────────────────
 GREEN='\033[0;32m'
