7d8377b2b6747aae9a11821dde132251a6387e68
Meko <137619369+Mekotofeuka@users.noreply.github.com>
2026-07-11 19:46:37 +0900

    Update proxymenu.sh

--- proxys/proxymenu.sh
+++ proxys/proxymenu.sh
@@ -15,11 +15,12 @@ NC='\033[0m'
 while true; do
     clear
     echo ""
-    echo -e "  ${BOLD}Прокси меню${NC}"
+    echo -e "  ${BOLD}Прокси меню v0.3${NC}"
     echo -e "  ${DIM}===========================${NC}"
     echo ""
     echo -e "  ${CYAN}[1]${NC}  ${BOLD}Меню работы с Telemt${NC}"
     echo -e "  ${CYAN}[2]${NC}  ${BOLD}Меню работы с MTProtoZig${NC}"
+    echo -e "  ${CYAN}[3]${NC}  ${BOLD}Меню работы с MTG${NC}"
     echo -e "  ${RED}[0]${NC}  ${BOLD}Назад в главное меню${NC}"
     echo ""
     echo -en "  ${BOLD}Выбор:${NC} "
@@ -42,6 +43,16 @@ while true; do
             else
                 echo ""
                 echo "  [✗] Файл /opt/mtpr-simple/proxys/mtprotozig1.sh не найден"
+                echo -e "  ${GRAY}Нажмите любую клавишу для возврата...${NC}"
+                read -rsn1
+            fi
+            ;;
+        3)
+            if [ -f "/opt/mtpr-simple/proxys/mtgv2_1.sh" ]; then
+                exec /opt/mtpr-simple/proxys/mtgv2_1.sh
+            else
+                echo ""
+                echo "  [✗] Файл /opt/mtpr-simple/proxys/mtgv2_1.sh не найден"
                 echo -e "  ${GRAY}Нажмите любую клавишу для возврата...${NC}"
                 read -rsn1
             fi
