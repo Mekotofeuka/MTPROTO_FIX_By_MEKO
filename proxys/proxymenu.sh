1a5ff14f9f502d1772b55cbf4d2e1b24121894e5
Meko <137619369+Mekotofeuka@users.noreply.github.com>
2026-07-14 02:52:20 +0900

    Update proxymenu.sh

--- proxys/proxymenu.sh
+++ proxys/proxymenu.sh
@@ -15,15 +15,17 @@ NC='\033[0m'
 while true; do
     clear
     echo ""
-    echo -e "  ${BOLD}Прокси меню v0.3${NC}"
+    echo -e "  ${BOLD}Меню движков и панелей Mtproto v0.32${NC}"
     echo -e "  ${DIM}===========================${NC}"
     echo ""
-    echo -e "  ${CYAN}[1]${NC}  ${BOLD}Меню работы с Telemt${NC}"
-    echo -e "  ${CYAN}[2]${NC}  ${BOLD}Меню работы с MTProtoZig${NC}"
-    echo -e "  ${CYAN}[3]${NC}  ${BOLD}Меню работы с MTG${NC}"
+    echo -e "  ${CYAN}[1]${NC}  ${BOLD}Меню Telemt${NC}"
+    echo -e "  ${CYAN}[2]${NC}  ${BOLD}Меню панелей для Telemt${NC}"
+    echo -e "  ${CYAN}[3]${NC}  ${BOLD}Меню MTProtoZig${NC}"
+    echo -e "  ${CYAN}[4]${NC}  ${BOLD}Меню MTG${NC}"
+	echo -e ""
     echo -e "  ${RED}[0]${NC}  ${BOLD}Назад в главное меню${NC}"
     echo ""
-    echo -en "  ${BOLD}Выбор:${NC} "
+    echo -en "  ${BOLD}Ввод:${NC} "
     read -r choice
 
     case "$choice" in
@@ -38,6 +40,42 @@ while true; do
             fi
             ;;
         2)
+            # Подменю панели Telemt
+            while true; do
+                clear
+                echo ""
+                echo -e "  ${BOLD}Меню панелей для Telemt${NC}"
+                echo -e "  ${DIM}===========================${NC}"
+                echo ""
+                echo -e "  ${CYAN}[1]${NC}  ${BOLD}Меню telemt_panel${NC}"
+                echo ""
+                echo -e "  ${RED}[0]${NC}  ${BOLD}Назад в прокси меню${NC}"
+                echo ""
+                echo -en "  ${BOLD}Ввод:${NC} "
+                read -r sub_choice
+
+                case "$sub_choice" in
+                    1)
+                        if [ -f "/opt/mtpr-simple/proxys/telemt_panel_amirotin.sh" ]; then
+                            exec /opt/mtpr-simple/proxys/telemt_panel_amirotin.sh
+                        else
+                            echo ""
+                            echo "  [✗] Файл /opt/mtpr-simple/proxys/telemt_panel_amirotin.sh не найден"
+                            echo -e "  ${GRAY}Нажмите любую клавишу для возврата...${NC}"
+                            read -rsn1
+                        fi
+                        ;;
+                    0)
+                        break
+                        ;;
+                    *)
+                        echo "  Неверный выбор"
+                        sleep 0.1
+                        ;;
+                esac
+            done
+            ;;
+        3)
             if [ -f "/opt/mtpr-simple/proxys/mtprotozig1.sh" ]; then
                 exec /opt/mtpr-simple/proxys/mtprotozig1.sh
             else
@@ -47,7 +85,7 @@ while true; do
                 read -rsn1
             fi
             ;;
-        3)
+        4)
             if [ -f "/opt/mtpr-simple/proxys/mtgv2_1.sh" ]; then
                 exec /opt/mtpr-simple/proxys/mtgv2_1.sh
             else
